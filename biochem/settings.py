00ad44277a11c4490fa11ea5902f5b335fed164a
Emehinola Samuel <emehinolasam01@gmail.com>
2020-01-05 04:00:31 -0800

    initial commit

--- biochem/settings.py
+++ biochem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3ta$w2hi9no*#zi&(y_xyupe!_(=x8knj@ce$bxfw4-923ks3@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
